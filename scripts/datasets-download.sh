2fe39bbdf1ca80e655e3fc2a2a63ca2ef562fa76
fissoreg <giancarlo.fissore@gmail.com>
2021-06-11 17:15:28 +0000

    Fixed datasets scripts.

--- scripts/datasets-download.sh
+++ scripts/datasets-download.sh
@@ -3,17 +3,11 @@ pip install gdown
 pip install pandas
 
 # kitti semantics (autonomous driving)
-<<<<<<< HEAD
 gdown https://drive.google.com/u/0/uc?id=1nj0lZwTd8uK71ulZngAY3gcugeMXTIWO&export=download && pid=$!
 wait $pid
 # tweets
 gdown https://drive.google.com/u/0/uc?id=1DWscOPs1Wi4-y98q6FASUFRjOaO0ggZU&export=download && pid=$!
 wait $pid
-=======
-gdown https://drive.google.com/u/0/uc?id=1nj0lZwTd8uK71ulZngAY3gcugeMXTIWO&export=download &
-# tweets
-gdown https://drive.google.com/u/0/uc?id=1DWscOPs1Wi4-y98q6FASUFRjOaO0ggZU&export=download
->>>>>>> datasets-2
 
 # unsplash nature
 python py_scripts/unsplash_download.py
